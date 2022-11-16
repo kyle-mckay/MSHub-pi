dcafc6981ae6c319af1c6c8c56a93c07599f39de
kyle-mckay <83886427+kyle-mckay@users.noreply.github.com>
2022-11-15 23:01:59 -0700

    ready to test docker setup

---
+++ setup.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+#region config
+    #0 = false
+    #1 = true
+    SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+    b_installdocker=1 #if true then docker will attempt to install on execution
+#endregion
+
+#region functions
+addtolog () {
+    #adds the passed string to a log file with a timestamp prefix
+    timestamp=`date "+%Y/%m/%d %H:%M:%S.%3N"` #add %3N as we want millisecond too
+    echo "$timestamp | $1" >> log.txt # add to log
+    echo $1
+}
+docker_helloworld () {
+    #attempts to run the offocial docker test image to verify it is installed before proceeding
+    dockertest=`sudo docker run hello-world`
+    #check if docker test passes
+    SUB='Hello from Docker!'
+    if [[ "$dockertest" == *"$SUB"* ]] 
+    then
+        addtolog "Docker test has passed"
+        b_testdocker=1
+    else
+        addtolog "Docker test has failed"
+        b_testdocker=0
+    fi
+}
+#endregion
+
+#region docker
+    #test docker
+        #confirm if docker is installed already before proceeding
+    #install docker
+        #https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
+        #set up repository
+            sudo apt-get update
+            sudo apt-get install \
+                ca-certificates \
+                curl \
+                gnupg \
+                lsb-release
+            sudo mkdir -p /etc/apt/keyrings
+            curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+            echo \
+            "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+            $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        #install docker engine
+            sudo apt-get update
+            sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin #latest
+            docker_helloworld
+#end region
+
+#region docker compose
+if $b_testdocker -eq 1
+then
+    # begin compose setups
+    compose="${SCRIPT_DIR}/docker/compose/"
+fi
+#endregion
